7c8aa6a8aa03cfbdfd6d51dc9aaec5f8f0565e52
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-07-17 15:01:26 -0500

    created functionality to write js to products page

--- js/products_object.js
+++ js/products_object.js
@@ -7,65 +7,65 @@ function makeProduct(name, image, description, price) {
 
 let product1 = new makeProduct(
 		'Fender Telecaster',
-		'img/products/f_tele',
+		'../img/products/f_tele.jpg',
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		'$1,19'
+		'$1,119'
 	);
 
 let product2 = new makeProduct(
 		'Fender Jazzmaster',
-		'img/products/f_jazz',
+		'../img/products/f_jazz.jpg',
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		'$1,19'
+		'$1,119'
 	);
 
 let product3 = new makeProduct(
 		'Fender Stratocaster',
-		'img/products/f_strat',
+		'../img/products/f_strat.jpg',
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		'$1,19'
+		'$1,119'
 	);
 
 let product4 = new makeProduct(
 		'Gibson Firebird',
-		'img/products/g_firebird',
+		'../img/products/g_firebird.jpg',
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		'$1,19'
+		'$1,119'
 	);
 
 let product5 = new makeProduct(
 		'Gibson Les Paul',
-		'img/products/g_les',
+		'../img/products/g_les.jpg',
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		'$1,19'
+		'$1,119'
 	);
 
 let product6 = new makeProduct(
 		'Gibson Memphis',
-		'img/products/g_memphis',
+		'../img/products/g_memphis.jpg',
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		'$1,19'
+		'$1,119'
 	);
 
 let product7 = new makeProduct(
 		'Martin Jason Isbell',
-		'img/products/m_jason',
+		'../img/products/m_jason.jpg',
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		'$1,19'
+		'$1,119'
 	);
 
 let product8 = new makeProduct(
 		'Martin StreetMaster',
-		'img/products/m_street,',
+		'../img/products/m_street.jpg',
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		'$1,19'
+		'$1,119'
 	);
 
 let product9 = new makeProduct(
 		'Martin Ukelele',
-		'img/products/m_uke',
+		'../img/products/m_uke.png',
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		'$1,19'
+		'$1,119'
 	);
 
 let productsArr = [product1, product2, product3, product4, product5, product6, product7, product8, product9];
